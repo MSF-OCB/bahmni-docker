3a17f12f555f5d3e289a85efe16577dafead576a
RdN-MSF <ramses.de.norre@brussels.msf.org>
2018-03-18 17:22:07 +0100

    The start script takes a mode argument now, the value is either passive or active.

--- bahmni/start.sh
+++ bahmni/start.sh
@@ -8,8 +8,24 @@ function restart_openmrs_delayed() {
 }
 
 ansible-playbook -i ${INVENTORY} /ansible/bahmni_start.yml
-${BAHMNI} start
-#${BAHMNI} update-config
 
-restart_openmrs_delayed &
+case $1 in
+active)
+  ${BAHMNI} start
+  #${BAHMNI} update-config
+  ${BAHMNI} setup-mysql-replication
+  ${BAHMNI} setup-postgresql-replication
+
+  restart_openmrs_delayed &
+
+  tail -F /var/log/openmrs/openmrs.log /var/log/bahmni-lab/bahmni-lab.log /var/log/bahmni-reports/bahmni-reports.log
+  ;;
+passive)
+  tail -F /var/log/mysqld.log
+  ;;
+*)
+  echo 'Use "active" or "passive" as parameter.'
+  exit 1
+
+esac
 
