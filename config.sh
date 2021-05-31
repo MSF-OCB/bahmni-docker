623d37d70a1977a715d6e925ef3c0a90af662add
munnavhai <munnabcis@gmail.com>
2021-05-31 13:50:36 +0200

    remove previous files

--- config.sh
+++
@@ -1,5 +0,0 @@
-INVENTORY=/inventory
-IMPL_SPEC_PLAYBOOK=/etc/bahmni-installer/deployment-artifacts/${BAHMNI_IMPL_NAME}_config/playbooks/all.yml
-ANSIBLE_RPM_URL="/$(rpm -q ansible)"
-BAHMNI="bahmni -i local --ansible_rpm_url ""${ANSIBLE_RPM_URL}"
-
